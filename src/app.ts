851810da0e8a23d7e563df62488b1d6bf3bc0a7b
SardineFish <Sardine@live.cn>
2018-11-20 19:00:49 +0800

    Update app

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,18 @@ import config from "../config/config";
 
 const app = new Koa();
 const router = new Router();
+router
+    .post("/update", (ctx, next) =>
+    {
+        console.log(ctx.request);
+    })
+    .get("/update", (ctx, next) =>
+    {
+        console.log(ctx);
+    });
+
 app
     .use(router.routes())
     .use(router.allowedMethods())
     .listen(config.server.port, config.server.host);
 
-router.post("/update", (ctx, next) =>
-{
-    console.log(ctx.request);   
-});
